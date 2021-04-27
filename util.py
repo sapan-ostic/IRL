c31060b9c1a0c429cf529a74b9db0b228fbc72f4
sapan-ostic <sapanagrawal777@gmail.com>
2021-04-27 16:44:48 -0700

    remove ptan

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import numpy as np
-import ptan
 import torch
 from torch import nn as nn
 import torch.nn.functional as F
@@ -178,25 +177,6 @@ class GRL():
             AvgReward += Reward/Nsamp
         self.env.close()
         print("Average over ", Nsamp, " samples = ", AvgReward)
-
-
-class Agent(ptan.agent.PolicyAgent):
-
-    @torch.no_grad()
-    def __call__(self, states):
-        if self.preprocessor is not None:
-            states = self.preprocessor(states)
-            if torch.is_tensor(states):
-                states = states.to(self.device)
-                if states.ndim == 1:
-                    states = states.view(1, -1)
-        probs_v = self.model(states)
-        if self.apply_softmax:
-            probs_v = torch.softmax(probs_v, dim=1)
-        probs = probs_v.data.cpu().numpy()
-        actions = self.action_selector(probs)
-        return np.array(actions)
-
 
 class PGN(nn.Module):
     def __init__(self, input_size, n_actions):
